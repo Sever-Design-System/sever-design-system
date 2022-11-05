b68de6a490743ea69ea27bf1d5250777480f10dd
Andrey Frolov <inham130@gmail.com>
2022-11-05 10:18:36 +0300

    webpack config

---
+++ packages/react/webpack.common.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src', 'index.tsx'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    assetModuleFilename: 'assets/[name][ext]',
+    clean: true
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.json'],
+    modules: [
+      path.join(__dirname, 'node_modules')
+    ],
+    plugins: [new TsconfigPathsPlugin({
+      extensions: ['.js', '.jsx', '.json', '.ts', '.tsx']
+    })],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: [
+          {
+            loader: 'ts-loader',
+            options: {
+              configFile: path.resolve(__dirname, 'tsconfig.json')
+            },
+          },
+        ],
+        exclude: /(node_modules)/
+      },
+      {
+        test: /\.svg/,
+        type: 'asset/resource',
+        include: path.resolve(__dirname, "src"),
+        generator: {
+          filename: "assets/[name][ext]",
+        }
+      }
+    ]
+  },
+  plugins: [new HtmlWebpackPlugin({
+    template: 'index.html'
+  })]
+};
