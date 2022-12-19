21d51eeab36fa29eeae8e973b67197ecfc4f6b47
Andrey Frolov <inham130@gmail.com>
2022-12-19 21:43:08 +0300

    chore(react): tests for Button.tsx

--- packages/react/Button/Button.tsx
+++ packages/react/Button/Button.tsx
@@ -2,41 +2,21 @@ import { FC } from 'react';
 
 export interface ButtonProps {
   /**
-   * Is this the principal call to action on the page?
-   */
-  primary?: boolean;
-  /**
-   * What background color to use
-   */
-  backgroundColor?: string;
-  /**
-   * How large should the button be?
-   */
-  size?: 'small' | 'medium' | 'large';
-  /**
    * Button contents
    */
   label: string;
-  /**
-   * Optional click handler
-   */
-  onClick?: () => void;
 }
 
 /**
  * Primary UI component for user interaction
  */
 const Button: FC<ButtonProps> = ({
-  primary = false,
-  size = 'medium',
-  backgroundColor,
   label,
   ...props
 }) => {
   return (
     <button
       type="button"
-      style={{ backgroundColor }}
       {...props}
     >
       {label}
