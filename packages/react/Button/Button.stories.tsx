79f81cdcdab313b0943913987f1f06d680101d06
andrew.frolov <inham130@gmail.com>
2023-03-16 21:34:21 +0300

    chore(react): button with icon story

--- packages/react/Button/Button.stories.tsx
+++ packages/react/Button/Button.stories.tsx
@@ -1,15 +1,12 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
 import Button from './Button';
+import { PlusIcon } from '../Icons';
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-  title: 'Example/Button',
+  title: 'Button',
   component: Button,
-  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
 } as ComponentMeta<typeof Button>;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
@@ -19,5 +16,24 @@ export const Primary = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
 Primary.args = {
   label: 'Button',
-  size: 'small'
+  size: 'medium',
+  appearance: 'primary',
+};
+
+export const WithLeftIcon = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+WithLeftIcon.args = {
+  label: 'Button',
+  size: 'medium',
+  appearance: 'primary',
+  IconLeft: PlusIcon(),
+};
+
+export const WithRightIcon = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+WithRightIcon.args = {
+  label: 'Button',
+  size: 'medium',
+  appearance: 'primary',
+  IconLeft: PlusIcon(),
 };
