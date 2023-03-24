56cd7ca5663f666beee4208c87c4bf5334a1cba6
Artur Ivanov <artur.dsgn@yandex.ru>
2023-03-24 23:10:35 +0300

    Revert "chore(react): button with icon story"
    This reverts commit 79f81cdcdab313b0943913987f1f06d680101d06.

--- packages/react/Button/Button.stories.tsx
+++ packages/react/Button/Button.stories.tsx
@@ -1,12 +1,15 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
 import Button from './Button';
-import { PlusIcon } from '../Icons';
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-  title: 'Button',
+  title: 'Example/Button',
   component: Button,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
 } as ComponentMeta<typeof Button>;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
@@ -16,24 +19,5 @@ export const Primary = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
 Primary.args = {
   label: 'Button',
-  size: 'medium',
-  appearance: 'primary',
-};
-
-export const WithLeftIcon = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
-WithLeftIcon.args = {
-  label: 'Button',
-  size: 'medium',
-  appearance: 'primary',
-  IconLeft: PlusIcon(),
-};
-
-export const WithRightIcon = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
-WithRightIcon.args = {
-  label: 'Button',
-  size: 'medium',
-  appearance: 'primary',
-  IconLeft: PlusIcon(),
+  size: 'small'
 };
