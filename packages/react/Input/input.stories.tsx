fc32e6351249895805b88838e4816852e1be651e
andrew.frolov <inham130@gmail.com>
2023-04-17 20:15:12 +0300

    feat: input story placeholder

--- packages/react/Input/input.stories.tsx
+++ packages/react/Input/input.stories.tsx
@@ -2,15 +2,12 @@ import { ComponentStory, ComponentMeta } from '@storybook/react';
 
 import { Input } from './Input';
 
-// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
   title: 'Input',
   component: Input,
 } as ComponentMeta<typeof Input>;
 
-// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template: ComponentStory<typeof Input> = (args) => <Input {...args} />;
 
 export const Primary = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
 Primary.args = {};
