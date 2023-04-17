8d9231a7b3cd393ffb74c664baba48e07aba618a
andrew.frolov <inham130@gmail.com>
2023-04-17 20:14:56 +0300

    feat: input props and variants placeholder

--- packages/react/Input/Input.tsx
+++ packages/react/Input/Input.tsx
@@ -2,10 +2,33 @@ import { FC } from 'react';
 import { styled } from '../stitches.config';
 
 export interface IInputProps {
-  size: 'small' | 'medium';
+  size?: 'small' | 'medium';
+  placeholder?: string;
+  value: string;
+  disabled?: boolean;
+  readonly?: boolean;
+  valid?: boolean;
+  helpMessage?: string;
+  label?: string;
+  ghost?: boolean;
 }
 
-const StyledInput = styled('input', {});
+const StyledInput = styled('input', {
+  variants: {
+    size: {
+      small: {},
+      medium: {},
+    },
+    disabled: {
+      true: {},
+      false: {},
+    },
+    readonly: {
+      true: {},
+      false: {},
+    },
+  },
+});
 
 export const Input: FC<IInputProps> = () => {
   return <StyledInput />;
